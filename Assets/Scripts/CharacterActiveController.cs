8d77734eb0fa637149267795315afbf8992c1b64
chasemc67 <chasemc67@gmail.com>
2020-06-05 16:11:40 -0700

    Main game pieces are placed and starting to work

---
+++ Assets/Scripts/CharacterActiveController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum CharacterState {
+        // attacking a target
+        Attacking,
+        // see a nearby enemy and walking towards
+        Homing,
+        // walking forwards
+        Walking
+}
+
+public class CharacterActiveController : MonoBehaviour
+{
+    CharacterController characterController;
+
+    CharacterState currentState;
+    float speed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        currentState = CharacterState.Walking;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(currentState) {
+            case CharacterState.Walking:
+            characterController.SimpleMove(transform.TransformDirection(Vector3.forward) * speed);
+                break;
+            case CharacterState.Homing:
+                break;
+            case CharacterState.Attacking:
+                break;
+            default:
+                break;
+        }
+    }
+}
